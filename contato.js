7212fccdfa1f30d678a477854ece35c7c8d96027
Rafael904 <1849rafael@gmail.com>
2024-12-22 19:28:12 -0300

    Meu portifólio

---
+++ contato.js
@@ -0,0 +1,16 @@
+var contato = document.querySelector("p#contato");
+function gmail() {
+    contato.innerHTML = `Gmail: 1849rafael@gmail.com.`
+}
+function whatsapp() {
+    contato.innerHTML = `Whatsapp: +55 (61) 99371-0678.`
+}
+function github() {
+    contato.innerHTML = `Perfil do GitHub: Rafael904.`
+}
+function linkedIn() {
+    contato.innerHTML = `Clique na imagem.`
+}
+function sair() {
+    contato.innerHTML = '<br>'
+}
---
+++ especilidades.js
@@ -0,0 +1,13 @@
+var descricao = document.querySelector("div#descricao");
+function html() {
+    descricao.innerHTML = `<p>É uma linguagem de marcação utilizada na construção de uma pagina web.<p>Tenho 9 meses de experiencia com essa tecnologia</p>`
+}
+function css() {
+    descricao.innerHTML = `<p>É um mecanismo de adicionar estilo a um documento web.<p>Tenho 9 meses de experiencia com essa tecnologia</p>`
+}
+function js() {
+    descricao.innerHTML = `<p>É uma linguagem de programação. Juntamente com HTML e CSS, é uma das três principis tecnologia web.<p>Tenho 7 meses de experiencia com essa tecnologia</p>`
+}
+function sair() {
+    descricao.innerHTML = `<p>/*Passe o mouse por cima de alguma imagem para ver a descrição.*/</p>`
+}
